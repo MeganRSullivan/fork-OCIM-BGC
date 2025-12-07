dd52bea093e5ad64dc3a651215e4cf5d117410da
MeganRSullivan <megan.sullivan1@uri.edu>
2025-12-07 19:16:37 +0000

    driver_transient_PCcycle_bk2: run opt OIF 1000yr
    transient_test_OIF_opt_PC_noAtm_1000y_from_reoptNature_with_dop_GM15_npp1

--- Code_withCellModel/driver/driver_transient_PCcycle_bk2.m
+++ Code_withCellModel/driver/driver_transient_PCcycle_bk2.m
@@ -23,14 +23,14 @@ addpath('../src/')
 
 % test1_eqPcycle_with_DOPl_gamma1pct_from_reoptNature_with_dop_GM15_npp1
 
-VerName = 'transient_test_OIF_equalbkCP_newsteadystate_PC_noAtm_1000y_from_reoptNature_with_dop_GM15_npp1_'; 		% optional version name. leave as an empty character array
+VerName = 'transient_test_OIF_opt_PC_noAtm_1000y_from_reoptNature_with_dop_GM15_npp1_'; 		% optional version name. leave as an empty character array
 					% or add a name ending with an underscore
 VerNum = '';		% optional version number for testing
 
 par.VerName = VerName;
 
-par.equalbkCP_flag = true ; % reset parameters to make remin rates equal for P and C 
-par.fertilize_flag = true; % if true, increase surface productivity for 1 year
+par.equalbkCP_flag = false ; % reset parameters to make remin rates equal for P and C 
+par.fertilize_flag = true ; % if true, increase surface productivity for 1 year
 par.resetbkCP_flag = false; % after solve steady state, reset remin parameters to original values before transient run
 
 % Choose C2P function
